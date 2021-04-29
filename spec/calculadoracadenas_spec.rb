9884571f8a1aee5d2dc1005d19a7e18b96ce0511
jerson <jersongarciavacaflor@gmail.com>
2021-04-28 23:06:18 -0400

    1ra prueba devuelve la constante requerida

---
+++ spec/calculadoracadenas_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe "Calculadora De Cadenas" do
+    it "Deberia devolver el numero 3 para la cadena 3" do
+        entrada="3"
+        expect(calcularCadena(entrada)).to eq(3)  
+    end
+end
+
+def calcularCadena(cad)
+    3
+end
