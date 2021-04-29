eedf313f2f198cf4e18e5436241905866ade59ad
jerson <jersongarciavacaflor@gmail.com>
2021-04-28 23:12:43 -0400

    2da prueba retorna convertida la cadena en numero

--- spec/calculadoracadenas_spec.rb
+++ spec/calculadoracadenas_spec.rb
@@ -1,10 +1,13 @@
 RSpec.describe "Calculadora De Cadenas" do
     it "Deberia devolver el numero 3 para la cadena 3" do
-        entrada="3"
-        expect(calcularCadena(entrada)).to eq(3)  
+        expect(calcularCadena("3")).to eq(3)
+    end
+
+    it "Deberia devolver el numero 5 para la cadena 5" do
+        expect(calcularCadena("5")).to eq(5)
     end
 end
 
 def calcularCadena(cad)
-    3
+    return cad.to_i
 end
